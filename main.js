74000c9fd34852154ae101b0b9fe2492b479e005
bbouley <b.bouley@hotmail.co.uk>
2015-07-27 14:14:05 -0600

    added tests for leap year

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 exports.outputHelloWorld = function () {
   return "Hello, world!";
 };
+
+exports.calculate = function(number){
+   if (number<=10){
+    var firstTen = (number/100) * 10;
+    return firstTen;
+  } else if (number <=20){
+    var secondTen = ((number-10)/100)*7 + 1;
+    return secondTen;
+  } else {
+    return ((number-20)/100)*5 + 1.7;
+  }
+};
+
+exports.leapYear = function(number){
+  if(number === 1700 || number === 1800|| number === 1900){
+    return false;
+  } else if(number%100 === 0 && number%400 !== 0 ){
+    return true;
+  } else if (number%4 === 0) {
+    return true;
+  }
+};
--- spec/spec.js
+++ spec/spec.js
@@ -1,14 +1,47 @@
 var code = require('../main.js');
 
-
-describe('', function() {
-  it('', function() {
-
-  });
-});
-
 describe("Hello World", function() {
   it("says 'hello, world!' when ran", function() {
     expect(code.outputHelloWorld()).toEqual("Hello, world!");
   });
+});
+
+describe('Tax Calculator', function(){
+
+  it('should tax 10% on the first $10', function() {
+    expect(code.calculate(1)).toEqual(0.1);
+    expect(code.calculate(10)).toEqual(1);
+  });
+
+  it('should tax 7% on the second $10', function(){
+    expect(code.calculate(15)).toEqual(1.35);
+  });
+
+  it('should tax 5% on the third $10', function(){
+    expect(code.calculate(30)).toEqual(2.2);
+    });
+  });
+
+describe('Leap Year Calculator', function(){
+
+    it('should return false', function(){
+      expect(code.leapYear(1700)).toEqual(false);
+      expect(code.leapYear(1800)).toEqual(false);
+      expect(code.leapYear(1900)).toEqual(false);
+    });
+
+    it('should return true', function(){
+      expect(code.leapYear(2000)).toEqual(true);
+      expect(code.leapYear(1600)).toEqual(true);
+    });
+
+    it ('should return true', function(){
+      expect(code.leapYear(1960)).toEqual(true);
+      expect(code.leapYear(1980)).toEqual(true);
+      expect(code.leapYear(1864)).toEqual(true);
+      expect(code.leapYear(1816)).toEqual(true);
+      expect(code.leapYear(1836)).toEqual(true);
+
+    });
+
 });
